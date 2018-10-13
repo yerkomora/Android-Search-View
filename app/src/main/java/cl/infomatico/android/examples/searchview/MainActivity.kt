c0925cb8e455049677599bb0d09dbc904025d999
Yerko Vasco Mora Sepúlveda <morayerko@gmail.com>
2018-10-13 14:22:31 -0300

    1.1
    Version 1.1.

--- app/src/main/java/cl/infomatico/android/examples/searchview/MainActivity.kt
+++ app/src/main/java/cl/infomatico/android/examples/searchview/MainActivity.kt
@@ -3,13 +3,12 @@ package cl.infomatico.android.examples.searchview
 import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
-import android.graphics.Rect
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.SearchView
 import android.view.Menu
 import android.view.MenuItem
-import android.view.ViewGroup
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,21 +18,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Keyboard
-
-        val rootView = findViewById<ViewGroup>(android.R.id.content)
-        rootView.viewTreeObserver.addOnGlobalLayoutListener {
-            val rect = Rect()
-            rootView.getWindowVisibleDisplayFrame(rect)
-
-            val screenHeight = rootView.height
-            val keypadHeight = screenHeight - rect.bottom
-
-            keyboardOpen = keypadHeight > screenHeight * 0.15
-        }
-
-
         handleIntent(intent)
     }
 
@@ -65,12 +49,6 @@ class MainActivity : AppCompatActivity() {
 
         searchView = searchItem.actionView as SearchView
 
-        searchView.setOnQueryTextFocusChangeListener { _, hasFocus ->
-            if (!hasFocus && !keyboardOpen && !buttonGoPress) {
-                searchItem.collapseActionView()
-            }
-        }
-
         // SearchManager
 
         val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
@@ -85,17 +63,16 @@ class MainActivity : AppCompatActivity() {
 
     private var text: String? = null
 
-    private var keyboardOpen = false
-    private var buttonGoPress = false
-
     private fun goMain() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
 
     private fun handleIntent(intent: Intent?) {
-        if (intent != null && Intent.ACTION_SEARCH == intent.action)
+        if (intent != null && Intent.ACTION_SEARCH == intent.action) {
             text = intent.getStringExtra(SearchManager.QUERY)
+            searchView.clearFocus()
+        }
 
         itemsLoad()
     }
